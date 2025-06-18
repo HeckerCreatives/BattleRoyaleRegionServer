f8472cc59b7816a9fc6a3462d78cc50b8bd34ff7
HeckerCreatives <daniel.janbiengabrielle@gmail.com>
2025-06-18 14:03:06 +0800

    Update notification.js

--- socket/web/notification.js
+++ socket/web/notification.js
@@ -4,7 +4,7 @@ const sendnotification = async (io, socket) => {
         console.log(`web has send a notification`)
         console.log(`socket server will trigger notification refresh to all clients`)
         
-        io.emit("receivenotification", "")
+        socket.broadcast.emit("receivenotification", "")
     })
 }
 
