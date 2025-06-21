f4a0dc56a46ece2c54c8ba9464198ea00a960173
HeckerCreatives <113580254+HeckerCreatives@users.noreply.github.com>
2025-06-21 16:11:09 +0800

    Update connectionHandler.js

--- socket/config/connectionHandler.js
+++ socket/config/connectionHandler.js
@@ -75,6 +75,7 @@ exports.eventconnection = (io, socket) => {
 
         activeUsers.set(currentUserId, socket.id);
         socket.join(currentUserId);
+        socket.emit("loggedin", "")
         startHeartbeat();
 
         console.log(`User ${currentUserId} logged in on ${socket.id}`);
